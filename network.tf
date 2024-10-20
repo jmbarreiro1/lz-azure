263d304841ecbb3c69b7fccfe4c622a33dd45072
jmbarreiro <jmbarreiro@gmail.com>
2024-10-20 20:56:57 +0200

    more things

---
+++ network.tf
@@ -0,0 +1,22 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-lz"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  address_space       = ["10.0.0.0/16"]
+  #dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name             = "sbn-services"
+    address_prefixes = ["10.0.1.0/24"]
+  }
+
+  subnet {
+    name             = "sbn-management"
+    address_prefixes = ["10.0.2.0/24"]
+    #security_group   = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
